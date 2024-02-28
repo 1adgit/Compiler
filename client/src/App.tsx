44b02856fbd4f5af9b05d78cc85bc84835f534a0
Aditya <adeo6509@gmail.com>
2024-02-28 14:14:35 +0530

    added frontend with shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, illum
+      voluptas quis ea ipsam ad, numquam ut et laboriosam tempora architecto
+      ducimus fugit possimus ullam suscipit praesentium adipisci, cupiditate
+      quod!
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
